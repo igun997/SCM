7f8599822c61f5abd38801bd301714bc8403b700
Indra Gunanda <indra.gunanda@gmail.com>
2019-12-16 15:57:49 -0800

    jenis

--- resources/views/franchise/mentor/franchise_layananform.blade.php
+++ resources/views/franchise/mentor/franchise_layananform.blade.php
@@ -29,6 +29,15 @@
                   <input type="text" class="form-control" name="harga" value="{{@$data->harga}}">
                 </div>
                 <div class="form-group">
+                  <label>Jenis</label>
+                  <select class="form-control" name="jenis">
+                    <option value="{{@$data->jenis}}" selected>{{@ucfirst($data->jenis)}}</option>
+                    <option value="tas">Tas</option>
+                    <option value="sepatu">Sepatu</option>
+                    <option value="helm">Helm</option>
+                  </select>
+                </div>
+                <div class="form-group">
                   <button type="submit" class="btn btn-success">Simpan Data</button>
                 </div>
               </form>
