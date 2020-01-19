be2a17cb047caab681a829a0fb5f1c2a34530035
Indra <indra.gunanda@gmail.com>
2020-01-19 13:30:24 +0700

    kwitansi

--- resources/views/invoice/head.blade.php
+++ resources/views/invoice/head.blade.php
@@ -1,11 +1,11 @@
 <table style="width:100%;border:0px !important;" >
   <tr style="border:0px !important;">
-    <td rowspan="3" style="border:0px !important;width:80px">
+    <td rowspan="3" style="border:0px !important;width:40px;padding:0px 0px 0px;">
       <center>
-        <img src="{{public_path("assets/images/logo.png")}}" style="width:80px;height: auto;" alt="">
+        <img src="{{public_path("assets/images/logo.png")}}" style="width:150px; height: auto; margin-right:20px;" alt="">
       </center>
     </td>
-    <td align="center" rowspan="3" style="border:0px !important;"><h2>WENOW CLEAN</h2><h3>Kp. Warung Domba RT 03/RW 01 No. 71 Ds. Mandalamukti<br>Kec. Cikalong Wetan Kab Bandung Barat</h3><h4>HP : 085103109169 / 081222702010 Email : bhaktinusantarabandung@gmail.com</h4></td>
+    <td align="center" rowspan="3" style="border:0px !important; margin-left:20px;"><h2>WENOW</h2><h3>Kp. Warung Domba RT 03/RW 01 No. 71 Ds. Mandalamukti<br>Kec. Cikalong Wetan Kab Bandung Barat</h3><h4>HP : 085103109169 / 081222702010 Email : bhaktinusantarabandung@gmail.com</h4></td>
   </tr>
   <tr style="border:0px !important;">
   </tr>
