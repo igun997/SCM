3b257e2a1e35c9736790e74d1da7894c393ce0e7
Indra <indra.gunanda@gmail.com>
2020-02-01 03:52:54 +0700

    Laporan Bahan Baku

--- resources/views/invoice/lapbb.blade.php
+++ resources/views/invoice/lapbb.blade.php
@@ -1,118 +1,93 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+<html>
+  <head>
     <title>Laporan Bahan Baku</title>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
     <style>
-        @page {
-          margin-top: 0px;
-          margin-bottom: 0px;
-        }
-        body{
-            font-family:'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
-            color:#333;
-            text-align:left;
-            font-size:13px;
-            margin:0;
-        }
-        .container{
-            margin:0 auto;
-            margin-top:35px;
-            padding:40px;
-            width:auto;
-            height:auto;
-            background-color:#fff;
-        }
-        caption{
-            font-size:28px;
-            margin-bottom:15px;
-        }
-        table{
-            border:1px solid #333;
-            border-collapse:collapse;
-            margin:0 auto;
-            width:auto;
-        }
-        td, tr, th{
-            padding:12px;
-            border:1px solid #333;
-            width:auto;
-        }
-        th{
-            background-color: #f0f0f0;
-        }
-        h4, p{
-            margin:0px;
-        }
+      td {
+        padding:3px 3px 3px;
+      }
+      .table_po {
+        padding:3px 3px 3px;
+        border-collapse: collapse;
+        border:1px solid;
+        width:100%
+      }
+      .table_po tr td{
+        border:1px solid;
+        text-align:center;
+      }
     </style>
-</head>
-<body>
-    <div class="container">
-      @include("invoice.head")
-        <h1 align="center">Laporan Bahan Baku</h1>
-        <h3  align="center">
-          Periode {{date("d-m-Y",strtotime($req["dari"]))}} - {{date("d-m-Y",strtotime($req["sampai"]))}}
-        </h3>
-        <br>
-        <table>
-            <thead>
-              <tr>
-                <th>No</th>
-                <th>Kode Bahan Baku</th>
-                <th>Nama Bahan Baku</th>
-                <th>Stok</th>
-                <th>Stok Minimum</th>
-                <th>Harga</th>
-                <th>Total Masuk</th>
-                <th>Total Keluar</th>
-                <th>Total Keluar (Hilang)</th>
-                <th>Tanggal Dibuat</th>
-              </tr>
-            </thead>
-            <tbody>
-              @foreach($data as $key => $value)
-              <tr>
-                <td>{{($key+1)}}</td>
-                <td>{{$value->id_bb}}</td>
-                <td>{{$value->nama}}</td>
-                <td>{{number_format($value->stok)}} {{$value->master_satuan->nama_satuan}}</td>
-                <td>{{number_format($value->stok_minimum)}} {{$value->master_satuan->nama_satuan}}</td>
-                <td>Rp. {{number_format($value->harga)}}</td>
-                <td>{{number_format($value->total_masuk($value->id_bb,date("d-m-Y",strtotime($req["dari"])),date("Y-m-d",strtotime($req["sampai"]))))}}</td>
-                <td>{{number_format($value->total_keluar($value->id_bb,date("d-m-Y",strtotime($req["dari"])),date("Y-m-d",strtotime($req["sampai"]))))}}</td>
-                <td>{{number_format($value->total_keluar_hilang($value->id_bb,date("d-m-Y",strtotime($req["dari"])),date("Y-m-d",strtotime($req["sampai"]))))}}</td>
-                <td>{{(($value->tgl_register == null)?"-":date("d-m-Y",strtotime($value->tgl_register)))}}</td>
-              </tr>
-              @endforeach
-            </tbody>
-            <tfoot>
-              <tr>
-                <th colspan="4" align="center">Ketua Divisi WENOW</th>
-                <td colspan="2" rowspan="3"></td>
-                <th colspan="4" align="center">Bag. Gudang</th>
-              </tr>
-              <tr>
-                <td colspan="4" style="height:100px">
-                  <center>
-                    <img src="{{(\App\Models\Pengguna::where(['level'=>"direktur"])->first()->ttd)}}" style="width:200px;height: auto;" alt="">
-                  </center>
-                </td>
+  </head>
+  <body>
+    @include("invoice.head")
+    <h2 align="center">LAPORAN DATA BAHAN BAKU</h2>
+    <h5 align="center">PERIODE : {{date("d-m-Y",strtotime($req["dari"]))}} - {{date("d-m-Y",strtotime($req["sampai"]))}}</h5>
+    <table class='table_po'>
+      <tr style="font-weight:bold">
+        <th>No</th>
+        <th>Kode Bahan Baku</th>
+        <th>Nama Bahan Baku</th>
+        <th>Stok</th>
+        <th>Stok Minimum</th>
+        <th>Harga</th>
+        <th>Total Masuk</th>
+        <th>Total Keluar</th>
+        <th>Total Keluar (Hilang)</th>
+        <th>Tanggal Dibuat</th>
+      </tr>
+      @foreach($data as $key => $value)
+      <tr>
+        <td>{{($key+1)}}</td>
+        <td>{{$value->id_bb}}</td>
+        <td>{{$value->nama}}</td>
+        <td>{{number_format($value->stok)}} {{$value->master_satuan->nama_satuan}}</td>
+        <td>{{number_format($value->stok_minimum)}} {{$value->master_satuan->nama_satuan}}</td>
+        <td>Rp. {{number_format($value->harga)}}</td>
+        <td>{{number_format($value->total_masuk($value->id_bb,date("d-m-Y",strtotime($req["dari"])),date("Y-m-d",strtotime($req["sampai"]))))}}</td>
+        <td>{{number_format($value->total_keluar($value->id_bb,date("d-m-Y",strtotime($req["dari"])),date("Y-m-d",strtotime($req["sampai"]))))}}</td>
+        <td>{{number_format($value->total_keluar_hilang($value->id_bb,date("d-m-Y",strtotime($req["dari"])),date("Y-m-d",strtotime($req["sampai"]))))}}</td>
+        <td>{{(($value->tgl_register == null)?"-":date("d-m-Y",strtotime($value->tgl_register)))}}</td>
+      </tr>
+      @endforeach
+    </table>
+    <br>
+    <br>
+    <table width="200px" style="float:left;">
+      <tr>
+        <td align="center"></td>
+      </tr>
+       <tr>
+        <td align="center">Direktur</td>
+      </tr>
+       <tr>
+        <td align="center" height="100px">
+          <center>
+            <img src="{{(\App\Models\Pengguna::where(['level'=>"direktur"])->first()->ttd)}}" style="width:200px;height: auto;" alt="">
+          </center>
+        </td>
+      </tr>
+       <tr>
+        <td align="center">Jatra Novianto</td>
+      </tr>
+    </table>
+    <table width="200px" style="float:right">
+      <tr>
+        <td align="center">Bandung, 29 September 2020</td>
+      </tr>
+       <tr>
+        <td align="center">Bag. Gudang</td>
+      </tr>
+       <tr>
+        <td align="center" height="100px">
+          <center>
+            <img src="{{(\App\Models\Pengguna::where(['level'=>"gudang"])->first()->ttd)}}" style="width:200px;height: auto;" alt="">
+          </center>
+        </td>
+      </tr>
+       <tr>
+        <td align="center">{{session()->get("nama")}}</td>
+      </tr>
+    </table>
 
-                <td colspan="4"  style="height:100px">
-                  <center>
-                    <img src="{{(\App\Models\Pengguna::where(['level'=>"gudang"])->first()->ttd)}}" style="width:200px;height: auto;" alt="">
-                  </center>
-                </td>
-              </tr>
-              <tr>
-                <th colspan="4" align="center">Jatra Novianto</th>
-                <th colspan="4" align="center">{{session()->get("nama")}}</th>
-              </tr>
-            </tfoot>
-        </table>
-    </div>
-</body>
+  </body>
 </html>
