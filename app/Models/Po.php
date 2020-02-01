46cc1d5242cfceb227e98965ec198faa5c61c44c
Indra Gunanda <indra.gunanda@gmail.com>
2020-02-01 19:03:26 +0700

    Init New Modules

--- app/Models/Po.php
+++ app/Models/Po.php
@@ -2,7 +2,7 @@
 
 /**
  * Created by Reliese Model.
- * Date: Mon, 04 Nov 2019 13:08:18 +0000.
+ * Date: Sat, 01 Feb 2020 19:00:16 +0700.
  */
 
 namespace App\Models;
@@ -19,8 +19,13 @@ use Reliese\Database\Eloquent\Model as Eloquent;
  * @property string $username
  * @property string $password
  * @property bool $status
+ * @property string $level
+ * @property int $pos_id
  * 
- * @property \Illuminate\Database\Eloquent\Collection $pos__pemesanans
+ * @property \App\Models\Po $po
+ * @property \Illuminate\Database\Eloquent\Collection $permintaans
+ * @property \Illuminate\Database\Eloquent\Collection $pos_barangs
+ * @property \Illuminate\Database\Eloquent\Collection $pos_registers
  *
  * @package App\Models
  */
@@ -30,7 +35,8 @@ class Po extends Eloquent
 	public $timestamps = false;
 
 	protected $casts = [
-		'status' => 'bool'
+		'status' => 'bool',
+		'pos_id' => 'int'
 	];
 
 	protected $hidden = [
@@ -43,11 +49,28 @@ class Po extends Eloquent
 		'alamat',
 		'username',
 		'password',
-		'status'
+		'status',
+		'level',
+		'pos_id'
 	];
 
-	public function pos__pemesanans()
+	public function po()
 	{
-		return $this->hasMany(\App\Models\PosPemesanan::class, 'id_pos');
+		return $this->hasOne(\App\Models\Po::class, 'id_pos', 'pos_id');
+	}
+
+	public function permintaans()
+	{
+		return $this->hasMany(\App\Models\Permintaan::class, 'pos_id');
+	}
+
+	public function pos_barangs()
+	{
+		return $this->hasMany(\App\Models\PosBarang::class, 'pos_id');
+	}
+
+	public function pos_registers()
+	{
+		return $this->hasMany(\App\Models\PosRegister::class, 'pos_id');
 	}
 }
--- app/Models/PosBarang.php
+++ app/Models/PosBarang.php
@@ -2,7 +2,7 @@
 
 /**
  * Created by Reliese Model.
- * Date: Mon, 04 Nov 2019 13:13:05 +0000.
+ * Date: Sat, 01 Feb 2020 18:57:35 +0700.
  */
 
 namespace App\Models;
@@ -12,37 +12,57 @@ use Reliese\Database\Eloquent\Model as Eloquent;
 /**
  * Class PosBarang
  * 
- * @property int $id_pb
+ * @property int $id
+ * @property int $pos_id
  * @property string $id_produk
- * @property int $id_pos
+ * @property float $stok
+ * @property float $harga
+ * @property \Carbon\Carbon $tgl_update
+ * @property \Carbon\Carbon $tgl_register
  * 
  * @property \App\Models\Po $po
  * @property \App\Models\MasterProduk $master_produk
+ * @property \App\Models\PosTransaksiDetail $pos_transaksi_detail
  *
  * @package App\Models
  */
 class PosBarang extends Eloquent
 {
-	protected $table = 'pos__barang';
-	protected $primaryKey = 'id_pb';
+	protected $table = 'pos_barang';
 	public $timestamps = false;
 
 	protected $casts = [
-		'id_pos' => 'int'
+		'pos_id' => 'int',
+		'stok' => 'float',
+		'harga' => 'float'
+	];
+
+	protected $dates = [
+		'tgl_update',
+		'tgl_register'
 	];
 
 	protected $fillable = [
+		'pos_id',
 		'id_produk',
-		'id_pos'
+		'stok',
+		'harga',
+		'tgl_update',
+		'tgl_register'
 	];
 
 	public function po()
 	{
-		return $this->belongsTo(\App\Models\Po::class, 'id_pos');
+		return $this->belongsTo(\App\Models\Po::class, 'pos_id');
 	}
 
 	public function master_produk()
 	{
 		return $this->belongsTo(\App\Models\MasterProduk::class, 'id_produk');
+	}
+
+	public function pos_transaksi_detail()
+	{
+		return $this->hasOne(\App\Models\PosTransaksiDetail::class);
 	}
 }
