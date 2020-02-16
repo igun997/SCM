5be186c358366ba9c85cf2fe3cdfc62485b5e006
Indra <indra.gunanda@gmail.com>
2020-02-16 07:39:10 +0700

    Rollback Response

--- app/Http/Middleware/JwtMiddleware.php
+++ app/Http/Middleware/JwtMiddleware.php
@@ -23,11 +23,11 @@ class JwtMiddleware extends BaseMiddleware
             $user = JWTAuth::parseToken()->authenticate();
         } catch (Exception $e) {
             if ($e instanceof \Tymon\JWTAuth\Exceptions\TokenInvalidException){
-                return response()->json(['status' => 'Token is Invalid'],400);
+                return response()->json(['status' => 'Token is Invalid']);
             }else if ($e instanceof \Tymon\JWTAuth\Exceptions\TokenExpiredException){
-                return response()->json(['status' => 'Token is Expired'],400);
+                return response()->json(['status' => 'Token is Expired']);
             }else{
-                return response()->json(['status' => 'Authorization Token not found'],400);
+                return response()->json(['status' => 'Authorization Token not found']);
             }
         }
         return $next($request);
